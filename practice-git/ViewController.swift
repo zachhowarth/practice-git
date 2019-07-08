e9e8d4392cd03c9f0488d8b36f282ba4a8f522a6
Zach Howarth <zachhowarth@gmail.com>
2019-07-08 15:18:20 -0500

    Initial Commit

---
+++ practice-git/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practice-git
+//
+//  Created by Zachary Howarth on 7/8/19.
+//  Copyright © 2019 Zachary Howarth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
